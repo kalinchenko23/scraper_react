cb7e31d4dcded5164f5cac50f5deb111e885ee12
Maksym Kalinchenko <kalinchenko.max@gmail.com>
2023-10-16 15:14:45 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [formData, setFormData] = useState({
+    channel : '',
+    hours : '',
+    keyword : ''
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+     
+  }
+
+  let form = <form className="querry" onSubmit={handleSubmit}>
+    <div className='form-row'>
+      <label htmlFor='channel'>channel</label>
+      <input type='text' id="channel"></input>
+      <label htmlFor='hours'>hours</label>
+      <input type='text' id="hours"></input>
+      <label htmlFor='keyword'>keyword</label>
+      <input type='text' id="keyword"></input>
     </div>
-  );
+    <button className='btn'>
+      Scrape
+    </button>
+
+  </form>
+  
+  return form
 }
 
 export default App;

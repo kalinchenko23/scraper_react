72036e9c0d47bdf5bb44878042eea17cc2e6254c
Max <kalinchenko.max@gmail.com>
2023-10-19 15:42:02 +0200

    updating data from the input form

--- src/App.js
+++ src/App.js
@@ -9,20 +9,24 @@ function App() {
     keyword : ''
   });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
-     
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value }) 
   }
+  
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(formData);
+ 
+}
 
   let form = <form className="querry" onSubmit={handleSubmit}>
     <div className='form-row'>
       <label htmlFor='channel'>channel</label>
-      <input type='text' id="channel"></input>
+      <input type='text' id="channel" name="channel" onChange={handleChange}></input>
       <label htmlFor='hours'>hours</label>
-      <input type='text' id="hours"></input>
+      <input type='text' id="hours" name="hours" onChange={handleChange}></input>
       <label htmlFor='keyword'>keyword</label>
-      <input type='text' id="keyword"></input>
+      <input type='text' id="keyword" name="keyword" onChange={handleChange}></input>
     </div>
     <button className='btn'>
       Scrape
